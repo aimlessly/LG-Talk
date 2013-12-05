b9bbbe900f9a68839bcf0ba890c9c3c1d0c883da
amika <aiminggg@gmail.com>
2013-12-05 19:37:57 +0700

    Revert b05d79e..ebd7e26
    This rolls back to commit b05d79e688df8bb49bb25992975f48754c5d64f0.

--- application/views/chat.php
+++ application/views/chat.php
@@ -13,78 +13,26 @@
     <!--                           TAB 1 Home                               -->
     <!-- ****************************************************************** -->
       <div class="tab-pane active" id="home">
-		<div class="container">
-		
+      
+        <div class="container">
 
             <!-- Left Column -->
             <div class="span5 offset1">
-			
-			<!--form-->
-			<form  method="post" accept-charset="utf-8" action="<?php echo site_url("main/show_main"); ?>" style='float:right;'>
-				<select id="sel_health" name="health">
-				<option value="" selected="selected">--เลือกประโบคสุขภาพ--</option>
-				<?php
-				$time = $this->typeTextModel->getHealth();
-				foreach ($time as $row)
-				{
-				?>
-					<option value="<?=$row->Sentence?>"><?=$row->Sentence?></option>
-				<?php } ?>
-				<td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
-				</select>
-			</form>
-			<form  method="post" accept-charset="utf-8" action="<?php echo site_url("main/show_main"); ?>" style='float:right;'>
-				<select id="sel_money" name="money">
-				<option value="" selected="selected">--เลือกประโบคการเงิน--</option>
-				<?php
-				$time = $this->typeTextModel->getMoney();
-				foreach ($time as $row)
-				{
-				?>
-					<option value="<?=$row->Sentence?>"><?=$row->Sentence?></option>
-				<?php } ?>
-				<td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
-				</select>
-			</form>
-			<form  method="post" accept-charset="utf-8" action="<?php echo site_url("main/show_main"); ?>" style='float:right;'>
-				<select id="sel_time" name="time">
-				<option value="" selected="selected">--เลือกประโบคเวลา--</option>
-				<?php
-				$time = $this->typeTextModel->getTime();
-				foreach ($time as $row)
-				{
-				?>
-					<option value="<?=$row->Sentence?>"><?=$row->Sentence?></option>
-				<?php } ?>
-				<td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
-				</select>
-			</form>
-			<form  method="post" accept-charset="utf-8" action="<?php echo site_url("main/show_main"); ?>" style='float:right;'>
-				<select id="sel_food" name="food">  
-				<option value="" selected="selected">--เลือกประโบคอาหาร--</option>
-				<?php
-				$rs = $this->typeTextModel->getRows();
-					foreach ($rs as $row)
-				{
-				?>
-					<option value="<?=$row->Sentence?>"><?=$row->Sentence?></option>
-				<?php } ?>
-				<td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
-				</select>
-			</form>
-			<!-- End form-->
-			
+
+
               <!-- Message Box -->
-                <textarea class="span6 pull-right" id="txtNewMessage" name="txtNewMessage"
-                          placeholder="พิมพ์ข้อความของคุณที่นี่.." rows="5" style="font-size: 24px; padding-top: 18px; "></textarea>
+                <textarea class="span8" id="txtNewMessage" name="txtNewMessage"
+                          placeholder="พิมพ์ข้อความของคุณที่นี่.." rows="5" style="font-size: 24px; padding-top: 18px"></textarea>
                 <h6 class="pull-right">เหลืออีก <span id="spanNumChars">320</span> ตัวอักษร</h6>
                 <button id="btnPost" class="btn btn-info">ส่งข้อความ <i class="glyphicon glyphicon-send"></i></button>
+
+
 
               <!-- List of Current User Posts -->
               <?php if ( $max_posts ) : ?>
 
-                <div class="span9 row well" >
-                  <table id="tblMyMessages" class="table table-condensed table-hover"style='float:right;'>
+                <div class="span9 row well">
+                  <table id="tblMyMessages" class="table table-condensed table-hover">
                     
                     <thead>
                     <tr>
@@ -113,7 +61,7 @@
               <?php endif; ?>
 
               </div> <!-- End Left Column -->
-        </div> <!-- End Container -->
+            </div> <!-- End Container -->
 
 
 
